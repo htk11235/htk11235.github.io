1a30fad5f4d92f45c32671dfaf86394ffe8e60b3
Huynh Trong Khoa <htk11235@gail.com>
2021-04-11 15:23:50 +0700

    11/4

---
+++ baitap/js/jsbuoi2.js
@@ -0,0 +1,28 @@
+function checkname() {
+    let text, x = document.getElementById("itemName").value;
+    let name = x.split('');    
+    if ( name.length >= 10 || Number(name[0])) {
+        text = "Name is invalid";
+    }  else {
+        text=""; }
+    console.log( );
+    if ( x.length < 1 ) 
+        text ="Name is required";
+    document.getElementById("invalid").innerHTML = text ;
+}
+function Valid2() { 
+    let y,text2;
+    y = document.getElementById("Category").value;
+    if ( y = "0") {
+        text2 = "Category is required";
+    }
+    document.getElementById("invalid2").innerHTML = text2 ;
+}
+let loadFile = function(event) {
+    let reader = new FileReader();
+    reader.onload = function(){
+      let output = document.getElementById('output');
+      output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
